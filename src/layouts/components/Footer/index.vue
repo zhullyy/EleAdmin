0e21e6ae1ac9e3fe1e9bcc1798c096616ff51cd6
zll <zll1299838755@163.com>
2023-01-29 11:45:27 +0800

    init

---
+++ src/layouts/components/Footer/index.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="footer flx-center">
+		<a href="" target="_blank"> 2022 © wb-Admin </a>
+	</div>
+</template>
+
+<style scoped lang="scss">
+@import "./index.scss";
+</style>
